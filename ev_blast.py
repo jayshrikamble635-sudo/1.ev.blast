08c1e0348c3c543823dae0510ab040582400ddae
jayshrikamble635-sudo <jayshrikamble635@gmail.com>
2026-02-22 14:22:57 +0530

    Update ev_blast.py

--- ev_blast.py
+++ ev_blast.py
@@ -15,18 +15,15 @@ battery_temp = st.number_input("Battery Temperature", min_value=0.0)
 voltage = st.number_input("Voltage", min_value=0.0)
 current = st.number_input("Current", min_value=0.0)
 charging_cycles = st.number_input("Charging Cycles", min_value=0)
-
-if st.button("Predict"):
-
-    input_data = pd.DataFrame({
+input_data = pd.DataFrame({
         "battery_temp": [battery_temp],
         "voltage": [voltage],
         "current": [current],
         "charging_cycles": [charging_cycles]
     })
 input_data.columns = model.feature_names_in_
+if st.button("Predict"):
     prediction = model.predict(input_data)
-
     if prediction[0] == 1:
         st.error("High Risk of EV Blast")
     else:
